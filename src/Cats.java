827c04137afd44af7a1f095dbce0d64ff2a0318f
ScorpionTracer <badik-9494@mail.ru>
2023-01-24 19:38:46 +0500

    Create hw

--- src/Cats.java
+++ src/Cats.java
@@ -1,2 +1,25 @@
-public class Cats {
+public class Cats extends Animal {
+
+  private static final int MAX_LENGTH_RUN = 200;
+  private static final int MAX_LENGTH_JUMP = 2;
+  private static final boolean MAX_LENGTH_SWIM = false;
+
+  public Cats(int lengthRun, int lengthJump, int lengthSwim) {
+    super(lengthRun, lengthJump, lengthSwim);
+  }
+
+  @Override
+  public void run(int length) {
+    System.out.println("run: " + (length < MAX_LENGTH_RUN));
+  }
+
+  @Override
+  public void jump(double length) {
+    System.out.println("jump: " + (length < MAX_LENGTH_JUMP));
+  }
+
+  @Override
+  public void swim(int length) {
+    System.out.println("swim: " + MAX_LENGTH_SWIM);
+  }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,14 @@
+import java.util.Random;
+
 public class Test {
+  public static Random random = new Random();
+
+  public static void main(String[] args) {
+
+    var cat1 = new Cats(random.nextInt(500), random.nextInt(500), random.nextInt(500));
+    var dog1 = new Dogs(random.nextInt(500), random.nextInt(500), random.nextInt(500));
+
+    cat1.jump(100);
+    dog1.run(100);
+  }
 }
